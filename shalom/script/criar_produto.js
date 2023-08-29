1858bcdffd17ec47fe0544bdbbe650c5ca8267a7
pedroaugsuto14 <pedro52998@gmail.com>
2023-08-28 21:26:13 -0300

    resolvendo dsainer

--- shalom/script/criar_produto.js
+++ shalom/script/criar_produto.js
@@ -24,9 +24,10 @@ function criar_produto(){
         <div class="vitrine">
             <h2>`+val.nome+`</h2>
             <a href="`+val.link+`"><img src="`+val.img+`" alt="dsgfsg"></a>
-            <p class="istilo_resumo"> dbf fabfva ibgias dubgas bgafbg mdf bjhmdfi hgdbdfb dbnjk bnda sfvbgf oyhdsb guj</p>
             <p class="sifrao">R$</p>
             <p class="istilo_preco">`+val.preco+`</p>
+            <p class="botao_compra">ver</p>
+            <p class="istilo_resumo"> dbf fabfva ibgias dubgas bgafbg mdf bjhmdfi hgdbdfb dbnjk bnda sfvbgf oyhdsb guj</p>
         </div>
 
         `;
@@ -40,6 +41,7 @@ novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivf
 novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
 novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
 novo_produto('blusa' , 'midia/img/carrinho.png' , 0 , 45 , '#' , 'dfhgpd fuighaíd uf   dpf fdhbdfahd dfga dfga' , 'difugvba dfhbap pydfbau hsdbpauyh ')
+novo_produto("pedra" , "midia/img/carrinho.png" , 0, 30.00, '#', 'dvhpsdiuvgp vue vgervpewc uerfvuew fveg', 'fhweifvgeiv vhyevush ceyhvcewu c cewfvoweycv')
 
 
 criar_produto()
