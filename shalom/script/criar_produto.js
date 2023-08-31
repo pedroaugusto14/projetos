0eeba5994fc17a51b1d40d4b8e681dfaef446fad
pedroaugsuto14 <pedro52998@gmail.com>
2023-08-30 22:34:40 -0300

    projeto autidor
    provavelmente escrevi autidor errado

--- shalom/script/criar_produto.js
+++ shalom/script/criar_produto.js
@@ -1,6 +1,6 @@
 
 var conteiner= document.getElementById('produtos') 
-const lista = []
+const produtos = []
 
 function novo_produto (nome , img , quant , preco , link , desc , resumo){
     const produto = {
@@ -13,12 +13,12 @@ function novo_produto (nome , img , quant , preco , link , desc , resumo){
         resumo: resumo ,
     }
 
-    lista.push(produto)
+    produtos.push(produto)
 }
 
 //criando o produto
 function criar_produto(){
-    lista.map((val)=>{
+    produtos.map((val)=>{
         conteiner.innerHTML+= `
 
         <a href="#">
@@ -36,14 +36,29 @@ function criar_produto(){
     })
 }
 
-novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fdvbpdfuvbapduvbgáuvgbafívubafvbgaifsdugaioyf')
-novo_produto('zezinho' , 'midia/img/carrinho.png' , 0 , 600.00 , '#' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
-novo_produto('pedrinho' , 'midia/img/carrinho.png' , 0 , 9.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
-novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
-novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
-novo_produto('pedro' , 'midia/img/carrinho.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
-novo_produto('blusa' , 'midia/img/carrinho.png' , 0 , 45 , '#' , 'dfhgpd fuighaíd uf   dpf fdhbdfahd dfga dfga' , 'difugvba dfhbap pydfbau hsdbpauyh ')
-novo_produto("pedra" , "midia/img/carrinho.png" , 0, 30.00, '#', 'dvhpsdiuvgp vue vgervpewc uerfvuew fveg', 'fhweifvgeiv vhyevush ceyhvcewu c cewfvoweycv')
-
+novo_produto('pedro' , 'midia/img/produto_03.png' , 0 , 40.00 , '#' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fdvbpdfuvbapduvbgáuvgbafívubafvbgaifsdugaioyf')
+novo_produto('zezinho' , 'midia/img/produto_09.png' , 0 , 600.00 , '#' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
+novo_produto('pedrinho' , 'midia/img/produto_03.png' , 0 , 9.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
+novo_produto('pedro' , 'midia/img/produto_08.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' , 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
+novo_produto('pedro' , 'midia/img/produto_09.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
+novo_produto('pedro' , 'midia/img/produto_03.png' , 0 , 40.00 , '#', 'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,'fgbvfibvdivfuvfubvgfvbubdebnfubvujiauvadpsdbv' ,)
+novo_produto('blusa' , 'midia/img/produto_10.png' , 0 , 45 , '#' , 'dfhgpd fuighaíd uf   dpf fdhbdfahd dfga dfga' , 'difugvba dfhbap pydfbau hsdbpauyh ')
+novo_produto("pedra" , "midia/img/produto_08.png" , 0, 30.00, '#', 'dvhpsdiuvgp vue vgervpewc uerfvuew fveg', 'fhweifvgeiv vhyevush ceyhvcewu c cewfvoweycv')
 
 criar_produto()
+
+
+const autidores = []
+
+function criar_autidor(nome, preco, desconto, img, link, net) {
+    const autidor = {
+        nome: nome,
+        preco: preco,
+        desconto: desconto,
+        img: img,
+        link: link,
+        net: net,
+    }
+
+    autidores.push(autidor)
+}
